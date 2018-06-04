1515efa817fd737fcd39390d6d87d4d79539d089
Matthias Arzt <arzt@mpi-cbg.de>
2018-06-04 20:55:37 +0200

    Solve CreateTableExercise

--- src/main/java/de/mpicbg/learnathon/course/legacy/exercise4_tables/CreateTableExercise.java
+++ src/main/java/de/mpicbg/learnathon/course/legacy/exercise4_tables/CreateTableExercise.java
@@ -2,31 +2,42 @@ package de.mpicbg.learnathon.course.legacy.exercise4_tables;
 
 import ij.measure.ResultsTable;
 import net.imagej.ImageJ;
+import net.imagej.table.DefaultGenericTable;
+import net.imagej.table.DoubleColumn;
+import net.imagej.table.GenericColumn;
+import net.imagej.table.GenericTable;
 
 public class CreateTableExercise
 {
 	public static void main(String... args) {
 
-		new ImageJ().ui().showUI();
-		ResultsTable table = new ResultsTable();
+		ImageJ imagej = new ImageJ();
 
-		// we fill the table row by row with information about the largest towns in the world.
-		table.incrementCounter();
-		table.addValue("Town", "Shanghai");
-		table.addValue("Population", 24256800.0);
+		GenericColumn nameColumn = new GenericColumn("Town");
+		DoubleColumn populationColumn = new DoubleColumn("Population");
 
-		table.incrementCounter();
-		table.addValue("Town","Karachi");
-		table.addValue("Population", 23500000.0);
+		// we fill the columns with information about the largest towns in the world.
+		nameColumn.add("Karachi");
+		populationColumn.add(23500000.0);
 
-		table.incrementCounter();
-		table.addValue("Town","Bejing");
-		table.addValue("Population",21516000.0);
+		nameColumn.add("Bejing");
+		populationColumn.add(21516000.0);
 
-		table.incrementCounter();
-		table.addValue("Town","Sao Paolo");
-		table.addValue("Population",21292893.0);
+		nameColumn.add("Sao Paolo");
+		populationColumn.add(21292893.0);
 
-		table.show( "Results" );
+		// but actually, the largest town is Shanghai,
+		// so let's add it at the beginning of the table.
+		nameColumn.add(0, "Shanghai");
+		populationColumn.add(0, 24256800.0);
+
+		// After filling the columns, you can create a table
+		GenericTable table = new DefaultGenericTable();
+
+		// and add the columns to that table
+		table.add(nameColumn);
+		table.add(populationColumn);
+
+		imagej.ui().show( table );
 	}
 }
